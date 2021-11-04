93ee76d27c1b33dbd2418d2c1fccb84a93ac2fce
Yana <yanaegorova69.3@gmail.com>
2021-11-04 02:41:43 +0200

    +

---
+++ js/templates/product-item.js
@@ -0,0 +1,22 @@
+export const item = (product) => {
+
+  return `
+    <li class="item">
+
+  <div class="product__block js_product glass glass-m" id="${product.id}">
+  <a href="product-page.html?id=${product.id}" class="product">
+    <div class="product__img-box">
+     <img src="${product.image}" alt="" class="img">
+     <p class="product__name js_product-name">${product.name}</p>
+    </div>
+  
+    <div class="product__text-box">
+    
+      <span class="product__price">$${(product.price).toFixed(2)}</span>
+    </div>
+   </a>
+   <button class="btn product__btn js_add-to-cart">buy it</button>
+</div>
+  </li>
+    `
+}
